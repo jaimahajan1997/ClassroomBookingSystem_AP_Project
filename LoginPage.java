fa89a51cea85f22da9471fc126d78c59960d75e5
Raghav16071 <raghav16071@iiitd.ac.in>
2017-10-20 02:00:36 +0530

    update: main app page

--- LoginPage.java
+++ LoginPage.java
@@ -1,11 +1,13 @@
 package ProjectCode;
 
 import javafx.application.Application;
+import javafx.scene.layout.BorderPane;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.collections.FXCollections;
 import javafx.scene.text.FontWeight;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
@@ -41,25 +43,25 @@ public class LoginPage extends Application
 	    	grid.setVgap(10);
 	    	grid.setPadding(new Insets(25, 25, 25, 25));
 	    	
-			Image image = new Image("File:C:\\Users\\Raghav\\Desktop\\my.png");
+			Image image = new Image("File:C:\\Users\\Raghav\\Desktop\\logo.jpg");
 			ImageView imageView=new ImageView(image);
 			imageView.setFitHeight(200);
 			imageView.setFitWidth(600);
 			grid.add(imageView,1,0);
 
 	    	
-	    	Text scenetitle = new Text("        Room Booking System");
-	    	scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 40));
-	    	grid.add(scenetitle, 1, 2);
-	    	grid.setStyle("-fx-background-color: #d65959;");
-	    	Label userName = new Label("UserID:");
+	    	grid.setStyle("-fx-background-color: #11c6bd;");
+	    	Label userName = new Label("UserID");
+	    	userName.setFont(Font.font("Tohema", FontWeight.BOLD, 20));
 	    	grid.add(userName, 0, 4);
 
 	    	TextField userTextField = new TextField();
 	    	grid.add(userTextField, 1, 4);
+	    	//String name=userTextField.getText();
 	    	
 
-	    	Label pw = new Label("Password:");
+	    	Label pw = new Label("Password");
+	    	pw.setFont(Font.font("Tohema", FontWeight.BOLD, 20));
 	    	grid.add(pw, 0, 6);
 
 	    	PasswordField pwBox = new PasswordField();
@@ -69,7 +71,7 @@ public class LoginPage extends Application
 	    	HBox hbBtn = new HBox(10);
 	    	hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
 	    	hbBtn.getChildren().add(btn);
-	    	grid.add(hbBtn, 1, 7);
+	    	grid.add(hbBtn, 1, 10);
 	    	
 	    	
 	    	btn.setOnAction(new EventHandler<ActionEvent>() 
@@ -78,11 +80,63 @@ public class LoginPage extends Application
 	    	    @Override
 	    	    public void handle(ActionEvent e)
 	    	    {
-	    	    	TextField err = new TextField();
-	    	        err.setText("UserID or Password did not match");
-	    	        err.setStyle("-fx-background-color: #a7f213;");
-	    	        grid.add(err, 1, 8);
-	    	        
+	    	    	BorderPane border = new BorderPane();
+	    	    	VBox m=new VBox();
+	    	    	
+//	    	    	border.setStyle("-fx-background-color: #11c6bd;");
+	    			Image image = new Image("File:C:\\Users\\Raghav\\Desktop\\logo.jpg");
+	    			ImageView imageView=new ImageView(image);
+	    			imageView.setFitHeight(275);
+	    			imageView.setFitWidth(350);
+	    			border.setTop(imageView);
+	    			//border.setPadding(new Insets(25, 25, 25, 25));
+	    	    	Scene scenic=new Scene(border,100,100);
+	    	    	
+//	    	    	GridPane grid2 = new GridPane();
+//	    	    	grid2.setAlignment(Pos.TOP_CENTER);
+	    	    	m.setStyle("-fx-background-color: #11c6bd;");
+//	    	    	grid2.setPadding(new Insets(25, 25, 25, 25));  	    	
+	    	    	
+	    	    	Label User1 = new Label("USER INFORMATION");
+	    	    	User1.setFont(Font.font("Tohema", FontWeight.BOLD, 30));
+	    	    	
+	    	    	m.setPadding(new Insets(25, 25, 25, 25));
+	    	    	Label user2 = new Label("Raghav Bhatia");
+	    	    	user2.setFont(Font.font("Tohema", FontWeight.NORMAL, 30));
+	    	    	
+    	    	
+	    	    	Label mytype = new Label("Student");
+	    	    	mytype.setFont(Font.font("Tohema", FontWeight.NORMAL, 30));
+	    	    	
+//
+	    	    	Label myID = new Label(userTextField.getText());
+	    	    	myID.setFont(Font.font("Tohema", FontWeight.NORMAL, 25));
+
+	    	    	m.getChildren().addAll(User1,user2,mytype,myID);
+	    	    	
+	    	    	
+	    	    	
+	    	    	
+	    	    	border.setLeft(m);
+	    	    	
+	    	    	
+	    	    	
+	    	    	
+	    	    	
+	    	    	
+	    	    	
+	    	    	
+	    	    	
+	    	    	
+	    	    	
+	    	    	
+	    	    	
+	    	    	
+	    	    	
+	    	    	
+	    			primaryStage.setScene(scenic);
+	    			
+	    			
 	    	        
 	    	    }
 	    	});
@@ -91,7 +145,7 @@ public class LoginPage extends Application
 	    	HBox hbBtn2 = new HBox(10);
 	    	hbBtn2.setAlignment(Pos.BOTTOM_LEFT);
 	    	hbBtn2.getChildren().add(reg);
-	    	grid.add(hbBtn2, 0, 7);
+	    	grid.add(hbBtn2, 0, 10);
 	    	
 	    	reg.setOnAction(new EventHandler<ActionEvent>() 
 	    	{
@@ -106,8 +160,14 @@ public class LoginPage extends Application
 	    	    	grid.setVgap(10);
 	    	    	grid.setPadding(new Insets(25, 25, 25, 25));
 	    	    	
+	    	    	Image image = new Image("File:C:\\Users\\Raghav\\Desktop\\logo.jpg");
+	    			ImageView imageView=new ImageView(image);
+	    			imageView.setFitHeight(200);
+	    			imageView.setFitWidth(400);
+	    			grid.add(imageView,1,0);
+	    	    	
 	    	    	Text scenetitle = new Text(" CREATE ACCOUNT");
-	    	    	scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 40));
+	    	    	scenetitle.setFont(Font.font("ALGERIAN", FontWeight.NORMAL, 40));
 	    	    	grid.add(scenetitle, 1, 2);
 	    	    	grid.setStyle("-fx-background-color: #d16314;");
 	    	    	
@@ -140,12 +200,13 @@ public class LoginPage extends Application
 	    	    	grid.add(pwBox, 1, 10);
 	    	    	
 	    	    	
-	    	    	Button btn = new Button("Register");
-	    	    	HBox hbBtn = new HBox(10);
-	    	    	hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-	    	    	hbBtn.getChildren().add(btn);
-	    	    	grid.add(hbBtn, 1, 11);
+	    	    	Button btn11 = new Button("Register");
+	    	    	HBox hbBtn11 = new HBox(10);
+	    	    	hbBtn11.setAlignment(Pos.BOTTOM_RIGHT);
+	    	    	hbBtn11.getChildren().add(btn11);
+	    	    	grid.add(hbBtn11, 1, 11);
 	    	    	
+
 	    	    	Scene scene = new Scene(grid, 100, 100,Color.RED);
 	    	    	register.setScene(scene);
 	    			register.setHeight(1000);
@@ -153,6 +214,38 @@ public class LoginPage extends Application
 	    			register.show();
 	    	    	register.setTitle("Room Booking System | Create Account");
 	    	    	register.show();
+	    	    	
+	    	    	btn11.setOnAction(new EventHandler<ActionEvent>() 
+	    	    	
+	    	 	{
+	    	    		@Override
+	    	    		public void handle(ActionEvent e)
+	    	    		{
+	    	    			GridPane grid1 = new GridPane();
+	    	    	    	grid1.setHgap(10);
+	    	    	    	grid1.setVgap(10);
+	    	    	    	Image image = new Image("File:C:\\Users\\Raghav\\Desktop\\logo.jpg");
+	    	    			ImageView imageView=new ImageView(image);
+	    	    			imageView.setFitHeight(200);
+	    	    			imageView.setFitWidth(400);
+	    	    			grid1.add(imageView,0,0);
+	    	    	    	grid1.setPadding(new Insets(25, 25, 25, 25));
+	    	    	    	Label thanks = new Label("Registration Complete!");
+	    	    	    	grid1.setAlignment(Pos.CENTER);
+	    	    	    	grid1.setStyle("-fx-background-color: #d16314;");
+	    	    	    	thanks.setFont(Font.font("Tahoma", FontWeight.NORMAL, 40));
+	    	    	    	Scene next=new Scene(grid1,100,100,Color.RED);
+	    	    	    	grid1.add(thanks, 0, 1);
+	    	    	    	Button btn12 = new Button("CLOSE");
+	    	    	    	HBox hbBtn12 = new HBox(10);
+	    	    	    	hbBtn12.setAlignment(Pos.BOTTOM_RIGHT);
+	    	    	    	hbBtn12.getChildren().add(btn12);
+	    	    	    	grid1.add(btn12, 1, 5);
+	    	    	    
+	    	    			register.setScene(next);
+	    	    			
+	    	    		}
+	    	 	});
 	    	    	
 	    	    }
 	    	});
