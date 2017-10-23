206f08ddf43972ae16ad458960efafe361be56f4
Jai Mahajan <jai16154@iiitd.ac.in>
2017-10-24 04:20:26 +0530

    Final Static GUI

--- ClassroomBookingSystem_AP_Project-master/ClassroomBookingSystem_AP_Project-master/LoginPage/src/controller/Controller.java
+++ ClassroomBookingSystem_AP_Project-master/ClassroomBookingSystem_AP_Project-master/LoginPage/src/controller/Controller.java
@@ -37,6 +37,8 @@ public class Controller implements Initializable {
 	}
 	@FXML
 	private Button btn2;
+	@FXML
+	private Button btnlogout;
 	
 	public void gotopage_1(ActionEvent event) throws IOException  {
 		Node node=(Node) event.getSource();
@@ -66,6 +68,18 @@ public class Controller implements Initializable {
 		Node node=(Node) event.getSource();
 		  Stage stage=(Stage) node.getScene().getWindow();
 		  Parent root = FXMLLoader.load(getClass().getResource("/application/homepage2.fxml"));
+		  Scene scene = new Scene(root);
+		  stage.setScene(scene);
+		  stage.show();
+		
+	}
+	@FXML
+	private Button btn22;
+	
+	public void gotoadmin(ActionEvent event) throws IOException  {
+		Node node=(Node) event.getSource();
+		  Stage stage=(Stage) node.getScene().getWindow();
+		  Parent root = FXMLLoader.load(getClass().getResource("/application/homepage3.fxml"));
 		  Scene scene = new Scene(root);
 		  stage.setScene(scene);
 		  stage.show();
@@ -113,7 +127,7 @@ public class Controller implements Initializable {
 		i=0;
 		Node node=(Node) event.getSource();
 		  Stage stage=(Stage) node.getScene().getWindow();
-		  Parent root = FXMLLoader.load(getClass().getResource("/application/viewroombookings.fxml"));
+		  Parent root = FXMLLoader.load(getClass().getResource("/application/viewbooking.fxml"));
 		  Scene scene = new Scene(root);
 		  stage.setScene(scene);
 		  stage.show();
@@ -122,17 +136,149 @@ public class Controller implements Initializable {
 	@FXML
 	private Button btn9;
 	
+	@FXML
+	private Button btn11;
+	
 	public void viewbookings2(ActionEvent event) throws IOException  {
 		i=1;
 		Node node=(Node) event.getSource();
 		  Stage stage=(Stage) node.getScene().getWindow();
-		  Parent root = FXMLLoader.load(getClass().getResource("/application/viewroombookings.fxml"));
+		  Parent root = FXMLLoader.load(getClass().getResource("/application/viewbooking.fxml"));
 		  Scene scene = new Scene(root);
 		  stage.setScene(scene);
 		  stage.show();
 		
 	}
+	@FXML
+	private Button btn10;
 	
+	public void selectCourse(ActionEvent event) throws IOException  {
+		i=1;
+		Node node=(Node) event.getSource();
+		  Stage stage=(Stage) node.getScene().getWindow();
+		  Parent root = FXMLLoader.load(getClass().getResource("/application/selectCourse.fxml"));
+		  Scene scene = new Scene(root);
+		  stage.setScene(scene);
+		  stage.show();
+		
+	}
+	@FXML
+	private Button btn12;
+	@FXML
+	private Button btn18;
+	
+	public void timetable(ActionEvent event) throws IOException  {
+		Node node=(Node) event.getSource();
+		  Stage stage=(Stage) node.getScene().getWindow();
+		  Parent root = FXMLLoader.load(getClass().getResource("/application/viewTimeTable.fxml"));
+		  Scene scene = new Scene(root);
+		  stage.setScene(scene);
+		  stage.show();
+		
+	}
+	@FXML
+	private Button btn13;
+	
+	@FXML
+	private Button btn17;
+	
+	public void courses(ActionEvent event) throws IOException  {
+		Node node=(Node) event.getSource();
+		  Stage stage=(Stage) node.getScene().getWindow();
+		  Parent root = FXMLLoader.load(getClass().getResource("/application/mycourses.fxml"));
+		  Scene scene = new Scene(root);
+		  stage.setScene(scene);
+		  stage.show();
+		
+	}
+	@FXML
+	private Button btn14;
+	@FXML
+	private Button btn19;
+	@FXML
+	private Button btn25;
+	public void profile(ActionEvent event) throws IOException  {
+		Node node=(Node) event.getSource();
+		  Stage stage=(Stage) node.getScene().getWindow();
+		  Parent root = FXMLLoader.load(getClass().getResource("/application/MyProfile.fxml"));
+		  Scene scene = new Scene(root);
+		  stage.setScene(scene);
+		  stage.show();
+		
+	}
+	@FXML
+	private Button btn15;
+	@FXML
+	private Button btn20;
+	@FXML
+	private Button btn26;
+	public void cpassword(ActionEvent event) throws IOException  {
+		Node node=(Node) event.getSource();
+		  Stage stage=(Stage) node.getScene().getWindow();
+		  Parent root = FXMLLoader.load(getClass().getResource("/application/ChangePassword.fxml"));
+		  Scene scene = new Scene(root);
+		  stage.setScene(scene);
+		  stage.show();
+		
+	}
+	@FXML
+	private Button btn16;
+	@FXML
+	private Button btn23;
+	public void bookroom(ActionEvent event) throws IOException  {
+		Node node=(Node) event.getSource();
+		  Stage stage=(Stage) node.getScene().getWindow();
+		  Parent root = FXMLLoader.load(getClass().getResource("/application/Book a Room.fxml"));
+		  Scene scene = new Scene(root);
+		  stage.setScene(scene);
+		  stage.show();
+		
+	}
+	@FXML
+	private Button btn21;
+	
+	public void reqresp(ActionEvent event) throws IOException  {
+		Node node=(Node) event.getSource();
+		  Stage stage=(Stage) node.getScene().getWindow();
+		  Parent root = FXMLLoader.load(getClass().getResource("/application/ViewResponses_Admin.fxml"));
+		  Scene scene = new Scene(root);
+		  stage.setScene(scene);
+		  stage.show();
+		
+	}
+	@FXML
+	private Button btn24;
+	public void allbookings(ActionEvent event) throws IOException  {
+		Node node=(Node) event.getSource();
+		  Stage stage=(Stage) node.getScene().getWindow();
+		  Parent root = FXMLLoader.load(getClass().getResource("/application/All Bookings_Admin.fxml"));
+		  Scene scene = new Scene(root);
+		  stage.setScene(scene);
+		  stage.show();
+		
+	}
+	@FXML
+	private Button btn27;
+	public void classroomavail(ActionEvent event) throws IOException  {
+		Node node=(Node) event.getSource();
+		  Stage stage=(Stage) node.getScene().getWindow();
+		  Parent root = FXMLLoader.load(getClass().getResource("/application/ClassRoomAvailability.fxml"));
+		  Scene scene = new Scene(root);
+		  stage.setScene(scene);
+		  stage.show();
+		
+	}
+	@FXML
+	private Button btn28;
+	public void allcourses(ActionEvent event) throws IOException  {
+		Node node=(Node) event.getSource();
+		  Stage stage=(Stage) node.getScene().getWindow();
+		  Parent root = FXMLLoader.load(getClass().getResource("/application/AllCourses.fxml"));
+		  Scene scene = new Scene(root);
+		  stage.setScene(scene);
+		  stage.show();
+		
+	}
 	@FXML
 	private Button b1;
 	public void goback1(ActionEvent event) throws IOException   {
