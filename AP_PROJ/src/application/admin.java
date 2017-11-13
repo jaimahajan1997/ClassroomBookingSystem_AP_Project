ce7bdf0bc93a7a4e3d5f2ca03b5fb0f7e0eca304
Raghav16071 <raghav16071@iiitd.ac.in>
2017-11-13 18:52:02 +0530

    Update admin.java

--- AP_PROJ/src/application/admin.java
+++ AP_PROJ/src/application/admin.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class admin extends user implements Serializable
 {
+	public static ArrayList<Request> reqs=new ArrayList<Request>();
 	public admin(String f,String l, String uid, char ut, String pass )
 	{
 		super(f,l,uid,ut,pass);
