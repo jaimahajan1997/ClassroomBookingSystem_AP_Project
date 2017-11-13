086ff18fc4a9b2691c8a707771ea23c5c6403e26
Jai Mahajan <jai16154@iiitd.ac.in>
2017-11-13 19:59:40 +0530

    Update student.java

--- AP_PROJ/src/application/student.java
+++ AP_PROJ/src/application/student.java
@@ -3,7 +3,6 @@ package controller;
 import java.util.*;
 public class student extends user
 {
-	private ArrayList<course> myCourses=new ArrayList<course>();
 	
 	public student(String f,String l, String uid, char ut, String pass )
 	{
