94a9cff1c12f5d413ccef6827afbefe7490ec2be
Jai Mahajan <jai16154@iiitd.ac.in>
2017-11-13 20:00:02 +0530

    Update faculty.java

--- AP_PROJ/src/application/faculty.java
+++ AP_PROJ/src/application/faculty.java
@@ -3,7 +3,6 @@ import java.io.*;
 import java.util.*;
 public class faculty extends user implements Serializable
 {
-	private ArrayList<course> myCourses=new ArrayList<course>();
 	public faculty(String f,String l, String uid, char ut, String pass )
 	{
 		super(f,l,uid,ut,pass);
